554486eb9d2fd091b97b28421aecf67517c2051e
Kunal Gurjar <you@example.com>
2020-08-09 16:56:53 +0530

    Updates

---
+++ precache-manifest.5e63a01b9c2d97e47a1489249119e336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56d2d7af9050aa48652d02defd06337",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "621fcb5e4c6a989bd2f4",
+    "url": "/Sorting-Visualizer/static/css/main.6106f901.chunk.css"
+  },
+  {
+    "revision": "279b7ebb33e5cfefaa7e",
+    "url": "/Sorting-Visualizer/static/js/2.5397ed27.chunk.js"
+  },
+  {
+    "revision": "621fcb5e4c6a989bd2f4",
+    "url": "/Sorting-Visualizer/static/js/main.a42207f7.chunk.js"
+  },
+  {
+    "revision": "bb456b206216dd26c6ed",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.7e78c47a.js"
+  }
+]);
